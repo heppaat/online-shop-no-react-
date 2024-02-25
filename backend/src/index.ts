baf59e550847ddef0be74e4e58d4bedde5b877fc
heppaat <pali.herrer@gmail.com>
2024-02-25 12:16:16 +0100

    databse setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,3 +2,9 @@ import express from "express";
 import { z } from "zod";
 import cors from "cors";
 import fs from "fs/promises";
+
+const server = express();
+
+server.use(cors());
+//header has content type (application json), express knows that the body has to be parsed as json
+server.use(express.json());
