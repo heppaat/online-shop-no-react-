423b3e123e64c2916f17bbac5afe823a8ffac325
heppaat <pali.herrer@gmail.com>
2024-02-25 16:47:20 +0100

    index.ts in http export safeFetch abstraction

---
+++ frontend/src/http/index.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+
+type Response<Type> =
+  | {
+      success: true;
+      status: number;
+      data: Type;
+    }
+  | {
+      success: false;
+      status: number | null;
+    };
+
+type Method = "GET" | "POST" | "DELETE" | "PATCH";
+
+export const safeFetch = async <Schema extends z.ZodTypeAny>(
+  url: string,
+  method: Method,
+  schema: Schema,
+  payload?: any
+): Promise<Response<z.infer<typeof schema>>> => {
+  try {
+    const response = await fetch(url, {
+      method,
+      headers: payload ? { "Content-Type": "application/JSON" } : {},
+      body: payload ? JSON.stringify(payload) : undefined,
+    });
+
+    if (response.status >= 500)
+      return { success: false, status: response.status };
+
+    if (response.status >= 400)
+      return { success: false, status: response.status };
+
+    const data = await response.json();
+    const result = schema.safeParse(data);
+
+    if (!result.success) return { success: false, status: response.status };
+
+    return { success: true, data: result.data, status: response.status };
+  } catch (error) {
+    return { success: false, status: null };
+  }
+};
